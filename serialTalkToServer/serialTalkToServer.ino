4d63d919e0ef36ad3ce4934eaff004186c1298ba
omerXracing <32936162+omerXracing@users.noreply.github.com>
2020-01-10 18:32:35 +0200

    Add files via upload
    arduino uno code

---
+++ serialTalkToServer/serialTalkToServer.ino
@@ -0,0 +1,42 @@
+#define ledPin 13
+
+String serverIn = "";
+bool stringComplete = false;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  serverIn.reserve(1024);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  if (stringComplete){
+    if ((serverIn == "ON") || (serverIn == "on") || (serverIn == "On") ){
+      Serial.println("Turn On");
+      digitalWrite(ledPin, HIGH); 
+    }
+    if ((serverIn == "OFF") || (serverIn == "off") || (serverIn == "Off")){
+      Serial.println("Turn Off");
+      digitalWrite(ledPin, LOW); 
+    }
+    else{
+      Serial.println("Unknown command");
+    }
+    serverIn = "";
+    stringComplete = false; 
+  }
+}
+
+void serialEvent(){
+  while (Serial.available()){
+    char inChar = (char)Serial.read();
+    if (inChar == '\n'){
+      stringComplete = true;
+    }else{
+      serverIn += inChar;
+    }
+    delay(5);
+  }
+}
